0ac8e280584ca41973d2c8da805b274bc71130b0
Tin-Chanahan <tin.chan@ku.th>
2025-05-06 04:37:14 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Lanningpage from '@/components/Lanningpage'
+import React from 'react'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Lanningpage/>
+    </div>
+  )
+}
---
+++ src/components/lung.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function lung() {
+  return (
+    <div className='flex-con items-center'>
+    <p
+
+    </div>
+  )
+}
